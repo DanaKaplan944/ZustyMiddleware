6f7ee7257cc85dddf8c507b22e50e8c86a542e3e
Dana Kaplan <danakaplan96@gmail.com>
2023-12-02 16:41:07 -0500

    metrics time added

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-export default function zustyMiddleware(createFunction) {
+export default function zustymiddleware(createFunction) {
   return (set, get) => {
     const store = createFunction(set, get);
-
     for (let key in store) {
       if (typeof store[key] === 'function') {
         let originalFunction = store[key];
@@ -9,20 +8,26 @@ export default function zustyMiddleware(createFunction) {
         store[key] = (...args) => {
           const prevState = get();
 
+          const startTime = performance.now();
+
           originalFunction(...args);
 
+          const endTime = performance.now();
+
           const nextState = get();
+
+          const actionCompleteTime = endTime - startTime;
 
           window.postMessage({
             body: 'actionAndStateSnapshot',
             action: key,
+            actionCompleteTime,
             prevState: JSON.stringify(prevState),
             nextState: JSON.stringify(nextState),
           });
         };
       }
     }
-
     return store;
   };
 }
