e0e7d163f22b3c1ce99417dc39739b3cae8d5cf2
Dana Kaplan <danakaplan96@gmail.com>
2023-12-07 14:25:15 -0500

    added store

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-export default function zustymiddleware(createFunction) {
+export default function zustyMiddleware(createFunction) {
   return (set, get) => {
     const store = createFunction(set, get);
+
+    const applicationStore = {};
+    for (const key in store) {
+      applicationStore[key] = '' + store[key] + '';
+    }
+
     for (let key in store) {
       if (typeof store[key] === 'function') {
         let originalFunction = store[key];
-
         store[key] = (...args) => {
           const prevState = get();
-
           const startTime = performance.now();
-
           originalFunction(...args);
-
           const endTime = performance.now();
-
           const nextState = get();
 
           const actionCompleteTime = endTime - startTime;
@@ -24,6 +25,7 @@ export default function zustymiddleware(createFunction) {
             actionCompleteTime,
             prevState: JSON.stringify(prevState),
             nextState: JSON.stringify(nextState),
+            store: JSON.stringify(applicationStore),
           });
         };
       }
